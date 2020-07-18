d31b4c6e6bc4e7e63d2e7539995d8809bab85a92
Tomasz Sypko <tsypko@gmail.com>
2020-07-18 22:12:20 +0200

    container component refactored

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./style.css";
 
-const Container = ({children}) => (
+const Container = ({ children }) => (
     <main className="container">
-        <h1 className="container__header">simple currency exchange</h1>
+
         {children}
     </main>
 )
