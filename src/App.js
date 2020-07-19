3995af2c23194cd304296bf39ab1663e4f22e3d7
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 18:34:35 +0200

    result refactored to state function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './Container';
 import Header from './Header';
 import Form from './Form';
-import {currencies} from './utils/currencies';
+import { currencies } from './utils/currencies';
 
 function App() {
+
+  const [result, setResult] = useState("");
+  const calculateResult = (amount, currencyFrom, currencyTo, currencyToName) => {
+    setResult(`${(amount * currencyFrom / currencyTo).toFixed(2)}  ${currencyToName}`);
+  }
+
   return (
     <Container>
       <Header title="simple currency exchange" />
-      <Form title="conventer" currencies={currencies} />
+      <Form
+        title="conventer"
+        currencies={currencies}
+        result={result}
+        calculateResult={calculateResult}
+      />
     </Container>
   );
 }
