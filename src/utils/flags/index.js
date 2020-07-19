1f4faedd3716ba8a5bdeecfd05ea3cb9076dad2e
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 15:04:09 +0200

    added new flags

---
+++ src/utils/flags/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import plnFlagImage from '../flags/Flag_of_Poland_2.svg'
+import euroFlagImage from '../flags/Flag_of_euro.svg'
+import usdFlagImage from '../flags/US_44_Star_Flag.svg'
+import gbpFlagImage from '../flags/Flag_of_the_United_Kingdom.svg'
+import chfFlagImage from '../flags/Flag_of_Switzerland.svg'
+import dkkFlagImage from '../flags/Flag_of_Denmark.svg'
+import norFlagImage from '../flags/Flag_of_Norway.svg'
+import sweFlagImage from '../flags/Flag_of_Sweden.svg'
+import rusFlagImage from '../flags/Flag_of_Russia.svg'
+import ukrFlagImage from '../flags/Flag_of_Ukraine.svg'
+import czkFlagImage from '../flags/Flag_of_the_Czech_Republic.svg'
+import "./style.css";
+
+export const plnFlag = () => (
+    <img
+        src={plnFlagImage}
+        alt="Flag of Poland"
+        className="flag"
+    />
+)
+export const euroFlag = () => (
+    <img
+        src={euroFlagImage}
+        alt="Flag of UE"
+        className="flag"
+    />
+)
+export const usdFlag = () => (
+    <img
+        src={usdFlagImage}
+        alt="Flag of USA"
+        className="flag"
+    />
+)
+export const gbpFlag = () => (
+    <img
+        src={gbpFlagImage}
+        alt="Flag of Great Britain"
+        className="flag"
+    />
+)
+export const chfFlag = () => (
+    <img
+        src={chfFlagImage}
+        alt="Flag of Switzerland"
+        className="flag"
+    />
+)
+export const dkkFlag = () => (
+    <img
+        src={dkkFlagImage}
+        alt="Flag of Denmark"
+        className="flag"
+    />
+)
+export const norFlag = () => (
+    <img
+        src={norFlagImage}
+        alt="Flag of Norway"
+        className="flag"
+    />
+)
+export const sweFlag = () => (
+    <img
+        src={sweFlagImage}
+        alt="Flag of Sweden"
+        className="flag"
+    />
+)
+export const rusFlag = () => (
+    <img
+        src={rusFlagImage}
+        alt="Flag of Russia"
+        className="flag"
+    />
+)
+export const ukrFlag = () => (
+    <img
+        src={ukrFlagImage}
+        alt="Flag of Ukraine"
+        className="flag"
+    />
+)
+export const czkFlag = () => (
+    <img
+        src={czkFlagImage}
+        alt="Flag of Czechia"
+        className="flag"
+    />
+)
