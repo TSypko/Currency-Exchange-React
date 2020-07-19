efd465b46e752510e3bf51f57ef94605ab245f28
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 10:46:08 +0200

    calculating result added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,39 @@ import Header from './Header';
 import Form from './Form';
 
 function App() {
+
+  const currencies = [
+    {
+      name: "Polish Zloty",
+      rate: 1,
+      flagLink: `images/Flag_of_Poland_2.svg"  alt="Flag of Poland"`
+    },
+    {
+      name: "Euro",
+      rate: 4.4532,
+      flagLink: `images/Flag_of_euro.svg"  alt="Flag of Euro"`
+    },
+    {
+      name: "US Dollar",
+      rate: 3.9395,
+      flagLink: `images/US_44_Star_Flag.svg"  alt="Flag of US"`
+    },
+    {
+      name: "British Pound",
+      rate: 4.9256,
+      flagLink: `images/Flag_of_the_United_Kingdom.svg"  alt="Flag of GB"`
+    },
+    {
+      name: "Swiss Franc",
+      rate: 4.1702,
+      flagLink: `images/Flag_of_Switzerland.svg"  alt="Flag of Swiss"`
+    },
+  ];
+
   return (
     <Container>
       <Header title="simple currency exchange" />
-      <Form title="conventer" />
+      <Form title="conventer" currencies={currencies} />
     </Container>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,57 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
-const Form = ({ title }) => (
-    <form className="form">
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">
-                {title}
+
+
+const Form = ({ title, currencies }) => {
+
+    const [currencyFromName, setCurrencyFrom] = useState("Polish Zloty");
+    const onSelectCurrencyFromChange = ({ target }) => setCurrencyFrom(target.value);
+
+    const [currencyToName, setCurrencyTo] = useState("Euro");
+    const onSelectCurrencyToChange = ({ target }) => setCurrencyTo(target.value);
+
+    const [amount, setAmount] = useState("");
+    const onSetAmountChange = ({ target }) => setAmount(+target.value);
+
+    const currencyFrom = currencies[currencies.findIndex(({ name }) => name === currencyFromName)];
+    const currencyTo = currencies[currencies.findIndex(({ name }) => name === currencyToName)];
+
+    const result = (amount * currencyFrom.rate * (1 / currencyTo.rate)).toFixed(2);
+
+    return (
+        <form className="form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">
+                    {title}
                 </legend>
-            <div className="form__fieldWrapper">
-                <div className="form__field">
-
-                    <label htmlFor="inputCurrencyInType" className="form__label">From:</label>
-                    <select name="inputCurrencyInType" id="inputCurrencyInType"
-                        className="form__input form__input--select">
-                        <option className="form__item" value="pln">Polish Zloty</option>
-                        <option className="form__item" value="eur">Euro</option>
-                        <option className="form__item" value="usd">US Dollar</option>
-                        <option className="form__item" value="gbp">British Pound</option>
-                        <option className="form__item" value="chf">Swiss Franc</option>
-                    </select>
+                <div className="form__fieldWrapper">
+                    <div className="form__field">
+                        <label htmlFor="inputCurrencyInType" className="form__label">From:</label>
+                        <select
+                            name="inputCurrencyInType"
+                            id="inputCurrencyInType"
+                            className="form__input form__input--select"
+                            value={currencyFromName}
+                            onChange={onSelectCurrencyFromChange}
+                        >
+                            <option className="form__item">Polish Zloty</option>
+                            <option className="form__item">Euro</option>
+                            <option className="form__item">US Dollar</option>
+                            <option className="form__item">British Pound</option>
+                            <option className="form__item">Swiss Franc</option>
+                        </select>
+                    </div>
+                    <div className="form__currencyInFlag"></div>
+                    <div className="form__field">
+                        <label htmlFor="outputCurrencyType" className="form__label">To:</label>
+                        <select
+                            name="outputCurrencyType"
+                            id="outputCurrencyType"
+                            className="form__input form__input--select"
+                            value={currencyToName}
+                            onChange={onSelectCurrencyToChange}
+                        >
+                            <option className="form__item">Polish Zloty</option>
+                            <option className="form__item">Euro</option>
+                            <option className="form__item">US Dollar</option>
+                            <option className="form__item">British Pound</option>
+                            <option className="form__item">Swiss Franc</option>
+                        </select>
+                    </div>
+                    <div className="form__currencyOutFlag"></div>
+                    <div className="form__field ">
+                        <label htmlFor="amount" className="form__label"
+                        >Amount:</label>
+                        <input
+                            id="amount"
+                            type="number"
+                            step="0.01"
+                            className="form__input form__input--amount"
+                            name="amountIn"
+                            value={amount}
+                            onChange={onSetAmountChange}
+                        />
+                    </div>
                 </div>
-                <div className="form__currencyInFlag"></div>
-                <div className="form__field">
-
-                    <label htmlFor="outputCurrencyType" className="form__label">To:</label>
-                    <select name="outputCurrencyType" id="outputCurrencyType"
-                        className="form__input form__input--select">
-                        <option className="form__item" value="pln">Polish Zloty</option>
-                        <option className="form__item" value="eur">Euro</option>
-                        <option className="form__item" value="usd">US Dollar</option>
-                        <option className="form__item" value="gbp">British Pound</option>
-                        <option className="form__item" value="chf">Swiss Franc</option>
-                    </select>
-                </div>
-                <div className="form__currencyOutFlag"></div>
-
-
-                <div className="form__field ">
-                    <label htmlFor="amount" className="form__label">Amount:</label>
-                    <input id="amount" type="number" step="0.01"
-                        className="form__input form__input--amount" name="amountIn" />
-
-                </div>
-            </div>
-            <div className="form__field form__field--result">
-                <p className="form__result">Result: <span className="form__result--value"></span></p>
-                <p className="form__note"><strong>IMPORTANT!</strong> exchange rates according to National Bank of
+                <div className="form__field form__field--result">
+                    <p className="form__result">Result: <span className="form__result--value">{result}</span></p>
+                    <p className="form__note"><strong>IMPORTANT!</strong> exchange rates according to National Bank of
                         Poland from 2020-06-24</p>
-            </div>
+                </div>
 
-        </fieldset>
-    </form>
-
-)
+            </fieldset>
+        </form>
+    )
+}
 
 export default Form;
