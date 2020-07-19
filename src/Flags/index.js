e40e079136bc514b4753b69ad291077ee520be10
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 12:18:41 +0200

    Created Flags component

---
+++ src/Flags/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import euroFlagImage from '../Flags/Flag_of_euro.svg'
+import "./style.css";
+
+export const EuroFlag = () => (
+    <img
+    src={euroFlagImage}
+    alt="Flag of UE"
+    className="flag"
+/>
+)
