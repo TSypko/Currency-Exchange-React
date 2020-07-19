4e8577c4f0067ce11e74e0a9a11da6816b511e0a
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 12:51:38 +0200

    Updates

---
+++ precache-manifest.4c250f8b4d49d5a520d9170a938a8ec0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c7a4bd3a2dcadef112502d35e28a61",
+    "url": "/Currency-Exchange-React/index.html"
+  },
+  {
+    "revision": "ae839898f76122579e8c",
+    "url": "/Currency-Exchange-React/static/css/main.84d6bc1e.chunk.css"
+  },
+  {
+    "revision": "5bdc26f3a6b3bdcd4ea5",
+    "url": "/Currency-Exchange-React/static/js/2.0838bf16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-Exchange-React/static/js/2.0838bf16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae839898f76122579e8c",
+    "url": "/Currency-Exchange-React/static/js/main.efa4d2b3.chunk.js"
+  },
+  {
+    "revision": "710e263279d1990923ee",
+    "url": "/Currency-Exchange-React/static/js/runtime-main.8e14dfba.js"
+  },
+  {
+    "revision": "5e76774336ff321342747bf509c15a5e",
+    "url": "/Currency-Exchange-React/static/media/Flag_of_Poland_2.5e767743.svg"
+  },
+  {
+    "revision": "69547e25f5fe58a8b78777dc82725e04",
+    "url": "/Currency-Exchange-React/static/media/Flag_of_Switzerland.69547e25.svg"
+  },
+  {
+    "revision": "56bbc4f2da4807e47a7dec98ee381df8",
+    "url": "/Currency-Exchange-React/static/media/Flag_of_euro.56bbc4f2.svg"
+  },
+  {
+    "revision": "68b4d8305c4f90577e6357bb0c247d29",
+    "url": "/Currency-Exchange-React/static/media/Flag_of_the_United_Kingdom.68b4d830.svg"
+  },
+  {
+    "revision": "1b6ffca9e2688080282da4be5c70e646",
+    "url": "/Currency-Exchange-React/static/media/US_44_Star_Flag.1b6ffca9.svg"
+  },
+  {
+    "revision": "83b03288dab2acca913c5ca3e68c819f",
+    "url": "/Currency-Exchange-React/static/media/background.83b03288.jpg"
+  }
+]);
