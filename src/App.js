4c77e461e6e3a89ab6c44ee86496ee0cc55ad9a2
Tomasz Sypko <tsypko@gmail.com>
2020-07-19 12:30:14 +0200

    Flags component & import refactored

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from './Container';
 import Header from './Header';
 import Form from './Form';
+import * as flag from './Flags';
+
 
 function App() {
 
@@ -10,32 +12,32 @@ function App() {
       name: "Polish Zloty",
       shortname: "PLN",
       rate: 1,
-      flagLink: `images/Flag_of_Poland_2.svg"  alt="Flag of Poland"`
+      flagImage: flag.plnFlag(),
     },
     {
       name: "Euro",
       shortname: "EUR",
       rate: 4.4532,
-      flagLink: `images/Flag_of_euro.svg"  alt="Flag of Euro"`
+      flagImage: flag.euroFlag(),
     },
     {
       name: "US Dollar",
       shortname: "USD",
       rate: 3.9395,
-      flagLink: `images/US_44_Star_Flag.svg"  alt="Flag of US"`
+      flagImage: flag.usdFlag(),
     },
     {
       name: "British Pound",
       shortname: "GBP",
       rate: 4.9256,
-      flagLink: `images/Flag_of_the_United_Kingdom.svg"  alt="Flag of GB"`
+      flagImage: flag.gbpFlag(),
     },
     {
       name: "Swiss Franc",
       shortname: "CHF",
       rate: 4.1702,
-      flagLink: `images/Flag_of_Switzerland.svg"  alt="Flag of Swiss"`
-    },
+      flagImage: flag.chfFlag(),
+    }
   ];
 
   return (
--- src/Flags/index.js
+++ src/Flags/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import plnFlagImage from '../Flags/Flag_of_Poland_2.svg'
 import euroFlagImage from '../Flags/Flag_of_euro.svg'
+import usdFlagImage from '../Flags/US_44_Star_Flag.svg'
+import gbpFlagImage from '../Flags/Flag_of_the_United_Kingdom.svg'
+import chfFlagImage from '../Flags/Flag_of_Switzerland.svg'
 import "./style.css";
 
-export const EuroFlag = () => (
+export const plnFlag = () => (
     <img
-    src={euroFlagImage}
-    alt="Flag of UE"
-    className="flag"
-/>
+        src={plnFlagImage}
+        alt="Flag of Poland"
+        className="flag"
+    />
+)
+export const euroFlag = () => (
+    <img
+        src={euroFlagImage}
+        alt="Flag of UE"
+        className="flag"
+    />
+)
+export const usdFlag = () => (
+    <img
+        src={usdFlagImage}
+        alt="Flag of USA"
+        className="flag"
+    />
+)
+export const gbpFlag = () => (
+    <img
+        src={gbpFlagImage}
+        alt="Flag of Great Britain"
+        className="flag"
+    />
+)
+export const chfFlag = () => (
+    <img
+        src={chfFlagImage}
+        alt="Flag of Switzerland"
+        className="flag"
+    />
 )
