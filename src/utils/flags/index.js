bc7998baa4f5f4afae4a2b40420a97171eb3a0ae
Tomasz Sypko <tsypko@gmail.com>
2020-07-22 22:18:18 +0200

    flag source file names changed

--- src/utils/flags/index.js
+++ src/utils/flags/index.js
@@ -1,91 +1,104 @@
 import React from 'react';
-import plnFlagImage from '../flags/Flag_of_Poland.svg'
-import euroFlagImage from '../flags/Flag_of_euro.svg'
-import usdFlagImage from '../flags/US_44_Star_Flag.svg'
-import gbpFlagImage from '../flags/Flag_of_the_United_Kingdom.svg'
-import chfFlagImage from '../flags/Flag_of_Switzerland.svg'
-import dkkFlagImage from '../flags/Flag_of_Denmark.svg'
-import norFlagImage from '../flags/Flag_of_Norway.svg'
-import sweFlagImage from '../flags/Flag_of_Sweden.svg'
-import rusFlagImage from '../flags/Flag_of_Russia.svg'
-import ukrFlagImage from '../flags/Flag_of_Ukraine.svg'
-import czkFlagImage from '../flags/Flag_of_the_Czech_Republic.svg'
+import plnFlagImage from '../flags/Polish Zloty.svg'
+import euroFlagImage from '../flags/Euro.svg'
+import usdFlagImage from '../flags/US Dollar.svg'
+import gbpFlagImage from '../flags/British Pound.svg'
+import chfFlagImage from '../flags/Swiss Franc.svg'
+import dkkFlagImage from '../flags/Danish krone.svg'
+import norFlagImage from '../flags/Norwegian krone.svg'
+import sweFlagImage from '../flags/Swedish krona.svg'
+import rusFlagImage from '../flags/Russian rouble.svg'
+import ukrFlagImage from '../flags/Ukrainian hryvnia.svg'
+import czkFlagImage from '../flags/Czech koruna.svg'
 import "./style.css";
 
-export const plnFlag = () => (
+export const plnFlag =
     <img
         src={plnFlagImage}
         alt="Flag of Poland"
         className="flag"
     />
-)
-export const euroFlag = () => (
+
+export const euroFlag =
     <img
         src={euroFlagImage}
         alt="Flag of UE"
         className="flag"
     />
-)
-export const usdFlag = () => (
+
+export const usdFlag =
     <img
         src={usdFlagImage}
         alt="Flag of USA"
         className="flag"
     />
-)
-export const gbpFlag = () => (
+
+export const gbpFlag =
     <img
         src={gbpFlagImage}
         alt="Flag of Great Britain"
         className="flag"
     />
-)
-export const chfFlag = () => (
+
+export const chfFlag =
     <img
         src={chfFlagImage}
         alt="Flag of Switzerland"
         className="flag"
     />
-)
-export const dkkFlag = () => (
+
+export const dkkFlag =
     <img
         src={dkkFlagImage}
         alt="Flag of Denmark"
         className="flag"
     />
-)
-export const norFlag = () => (
+
+export const norFlag =
     <img
         src={norFlagImage}
         alt="Flag of Norway"
         className="flag"
     />
-)
-export const sweFlag = () => (
+
+export const sweFlag =
     <img
         src={sweFlagImage}
         alt="Flag of Sweden"
         className="flag"
     />
-)
-export const rusFlag = () => (
+
+export const rusFlag =
     <img
         src={rusFlagImage}
         alt="Flag of Russia"
         className="flag"
     />
-)
-export const ukrFlag = () => (
+
+export const ukrFlag =
     <img
         src={ukrFlagImage}
         alt="Flag of Ukraine"
         className="flag"
     />
-)
-export const czkFlag = () => (
+
+export const czkFlag =
     <img
         src={czkFlagImage}
         alt="Flag of Czechia"
         className="flag"
     />
-)
+
+export {
+    plnFlagImage,
+    euroFlagImage,
+    usdFlagImage,
+    gbpFlagImage,
+    chfFlagImage,
+    dkkFlagImage,
+    norFlagImage,
+    sweFlagImage,
+    rusFlagImage,
+    ukrFlagImage,
+    czkFlagImage
+}
