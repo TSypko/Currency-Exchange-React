ceb64ef59475bd87648efca7d93fa573395cc95d
Tomasz Sypko <tsypko@gmail.com>
2020-07-22 20:36:36 +0200

    CurrencySelector refactored to use array map

--- src/CurrencySelector/index.js
+++ src/CurrencySelector/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { currencies } from '../utils/currencies';
 import "./style.css";
 
 const CurrencySelector = ({ labelText, value, onChange }) => (
@@ -10,17 +11,13 @@ const CurrencySelector = ({ labelText, value, onChange }) => (
             value={value}
             onChange={onChange}
         >
-            <option className="currencySelector__item">Polish Zloty</option>
-            <option className="currencySelector__item">Euro</option>
-            <option className="currencySelector__item">US Dollar</option>
-            <option className="currencySelector__item">British Pound</option>
-            <option className="currencySelector__item">Swiss Franc</option>
-            <option className="currencySelector__item">Danish krone</option>
-            <option className="currencySelector__item">Norwegian krone</option>
-            <option className="currencySelector__item">Swedish krona</option>
-            <option className="currencySelector__item">Russian rouble</option>
-            <option className="currencySelector__item">Ukrainian hryvnia</option>
-            <option className="currencySelector__item">Czech koruna</option>
+            {currencies.map(currency =>
+                <option
+                    className="currencySelector__item"
+                    key={currency.shortname}>
+                    {currency.name}
+                </option>)}
+
         </select>
     </label>
 );
