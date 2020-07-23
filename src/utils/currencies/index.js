68a921f61b602b33d0fac83fb2372c318a9d01e7
Tomasz Sypko <tsypko@gmail.com>
2020-07-23 18:36:11 +0200

    flags imported directly to currencies

--- src/utils/currencies/index.js
+++ src/utils/currencies/index.js
@@ -1,4 +1,16 @@
-import * as flag from '../flags';
+import plnFlagImage from '../flags/Polish Zloty.svg'
+import euroFlagImage from '../flags/Euro.svg'
+import usdFlagImage from '../flags/US Dollar.svg'
+import gbpFlagImage from '../flags/British Pound.svg'
+import chfFlagImage from '../flags/Swiss Franc.svg'
+import dkkFlagImage from '../flags/Danish krone.svg'
+import norFlagImage from '../flags/Norwegian krone.svg'
+import sweFlagImage from '../flags/Swedish krona.svg'
+import rusFlagImage from '../flags/Russian rouble.svg'
+import ukrFlagImage from '../flags/Ukrainian hryvnia.svg'
+import czkFlagImage from '../flags/Czech koruna.svg'
+import jpyFlagImage from '../flags/Japanese Yen.svg'
+import hrkFlagImage from '../flags/Croatian Kuna.svg'
 
 export const rateDate = "2020-07-19";
 
@@ -7,91 +19,91 @@ export const currencies = [
     name: "Polish Zloty",
     shortname: "PLN",
     rate: 1,
-    flagImage: flag.plnFlagImage,
+    flagImage: plnFlagImage,
     country: "Poland",
   },
   {
     name: "Euro",
     shortname: "EUR",
     rate: 4.4827,
-    flagImage: flag.euroFlagImage,
+    flagImage: euroFlagImage,
     country: "European Union",
   },
   {
     name: "US Dollar",
     shortname: "USD",
     rate: 3.9263,
-    flagImage: flag.usdFlagImage,
+    flagImage: usdFlagImage,
     country: "United States of America",
   },
   {
     name: "British Pound",
     shortname: "GBP",
     rate: 4.9331,
-    flagImage: flag.gbpFlagImage,
+    flagImage: gbpFlagImage,
     country: "Great Britain",
   },
   {
     name: "Swiss Franc",
     shortname: "CHF",
     rate: 4.1647,
-    flagImage: flag.chfFlagImage,
+    flagImage: chfFlagImage,
     country: "Switzerland",
   },
   {
     name: "Danish krone",
     shortname: "DKK",
     rate: 0.6021,
-    flagImage: flag.dkkFlagImage,
+    flagImage: dkkFlagImage,
     country: "Denmark",
   },
   {
     name: "Norwegian krone",
     shortname: "NOK",
     rate: 0.4230,
-    flagImage: flag.norFlagImage,
+    flagImage: norFlagImage,
     country: "Norway",
   },
   {
     name: "Swedish krona",
     shortname: "SEK",
     rate: 0.4338,
-    flagImage: flag.sweFlagImage,
+    flagImage: sweFlagImage,
     country: "Sweden",
   },
   {
     name: "Russian rouble",
     shortname: "RUB",
     rate: 0.0546,
-    flagImage: flag.rusFlagImage,
+    flagImage: rusFlagImage,
     country: "Russia",
   },
   {
     name: "Ukrainian hryvnia",
     shortname: "UAH",
     rate: 0.1435,
-    flagImage: flag.ukrFlagImage,
+    flagImage: ukrFlagImage,
     country: "Ukraine",
   },
   {
     name: "Czech koruna",
     shortname: "CZK",
     rate: 0.1678,
-    flagImage: flag.czkFlagImage,
+    flagImage: czkFlagImage,
     country: "Czechia",
   },
   {
     name: "Japanese Yen",
     shortname: "JPY",
     rate: 3.6002 / 100,
-    flagImage: flag.jpyFlagImage,
+    flagImage: jpyFlagImage,
     country: "Japan",
   },
   {
     name: "Croatian Kuna",
     shortname: "HRK",
     rate: 0.5904,
-    flagImage: flag.hrkFlagImage,
+    flagImage: hrkFlagImage,
     country: "Croatia",
   },
 ];
