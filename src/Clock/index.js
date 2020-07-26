039237a9e6682c912d1fa60742a6e5f2a2005eb5
Tomasz Sypko <tsypko@gmail.com>
2020-07-26 14:30:59 +0200

    Added interval refreshing to Clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css'
 
-const Clock = () => (
-    <div className="clock">
-        <p className="clock__paragraph">
-            {/* example only */}
-            Dzisiaj jest niedziela, 26 lipca, 12:54:33
-        </p>
-    </div>
-);
+const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const dateString = date.toLocaleDateString(undefined, { weekday: "long", day: "numeric", month: "long" })
+    const timeString = date.toLocaleTimeString();
+
+    return (
+        <div className="clock">
+            <p className="clock__paragraph">
+                Dzisiaj jest {dateString}, {timeString}
+            </p>
+        </div>
+    );
+}
 
 export default Clock;
