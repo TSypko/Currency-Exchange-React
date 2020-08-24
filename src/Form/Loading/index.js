9ed2956c63b62bf18d78c2bc8359859cd2b7f2ae
Tomasz Sypko <tsypko@gmail.com>
2020-08-24 13:57:53 +0200

    Loading component created

---
+++ src/Form/Loading/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactLoading from 'react-loading';
+
+const Loading = () => {
+    return (
+       <ReactLoading  type={"spokes"} color={"#00a2ff"}  height={'30%'} width={'30%'} />
+    )
+}
+
+export default Loading
