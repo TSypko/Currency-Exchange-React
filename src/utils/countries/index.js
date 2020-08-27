0b774ee0792cf1fa60377aee3216eaf8e0bb9ff4
Tomasz Sypko <tsypko@gmail.com>
2020-08-27 13:07:56 +0200

    added new currencies

--- src/utils/countries/index.js
+++ src/utils/countries/index.js
@@ -1,22 +1,36 @@
-import plnFlagImage from '../flags/Polish Zloty.svg'
-import euroFlagImage from '../flags/Euro.svg'
-import usdFlagImage from '../flags/US Dollar.svg'
-import gbpFlagImage from '../flags/British Pound.svg'
-import chfFlagImage from '../flags/Swiss Franc.svg'
-import dkkFlagImage from '../flags/Danish krone.svg'
-import norFlagImage from '../flags/Norwegian krone.svg'
-import sweFlagImage from '../flags/Swedish krona.svg'
-import rusFlagImage from '../flags/Russian rouble.svg'
-import czkFlagImage from '../flags/Czech koruna.svg'
-import jpyFlagImage from '../flags/Japanese Yen.svg'
-import hrkFlagImage from '../flags/Croatian Kuna.svg'
-import bgnFlagImage from '../flags/Bulgarian lev.svg'
-import hufFlagImage from '../flags/Hungarian forint.svg'
-import ronFlagImage from '../flags/Romanian leu.svg'
-import iskFlagImage from '../flags/Icelandic krona.svg'
-import cadFlagImage from '../flags/Canadian dollar.svg'
-import tryFlagImage from '../flags/Turkish lira.svg'
-import audFlagImage from '../flags/Australian dollar.svg'
+import plnFlagImage from '../flags/Polish Zloty.svg';
+import euroFlagImage from '../flags/Euro.svg';
+import usdFlagImage from '../flags/US Dollar.svg';
+import gbpFlagImage from '../flags/British Pound.svg';
+import chfFlagImage from '../flags/Swiss Franc.svg';
+import dkkFlagImage from '../flags/Danish krone.svg';
+import norFlagImage from '../flags/Norwegian krone.svg';
+import sweFlagImage from '../flags/Swedish krona.svg';
+import rusFlagImage from '../flags/Russian rouble.svg';
+import czkFlagImage from '../flags/Czech koruna.svg';
+import jpyFlagImage from '../flags/Japanese Yen.svg';
+import hrkFlagImage from '../flags/Croatian Kuna.svg';
+import bgnFlagImage from '../flags/Bulgarian lev.svg';
+import hufFlagImage from '../flags/Hungarian forint.svg';
+import ronFlagImage from '../flags/Romanian leu.svg';
+import iskFlagImage from '../flags/Icelandic krona.svg';
+import cadFlagImage from '../flags/Canadian dollar.svg';
+import tryFlagImage from '../flags/Turkish lira.svg';
+import audFlagImage from '../flags/Australian dollar.svg';
+import brlFlagImage from '../flags/Brazil.svg';
+import cnyFlagImage from '../flags/China.svg';
+import hkdFlagImage from '../flags/HongKong.svg';
+import idrFlagImage from '../flags/Indonesia.svg';
+import ilsFlagImage from '../flags/Israel.svg';
+import inrFlagImage from '../flags/India.svg';
+import krwFlagImage from '../flags/SouthKorea.svg';
+import mxnFlagImage from '../flags/Mexico.svg';
+import myrFlagImage from '../flags/Malaysia.svg';
+import nzdFlagImage from '../flags/NewZealand.svg';
+import phpFlagImage from '../flags/Philippines.svg';
+import sgdFlagImage from '../flags/Singapore.svg';
+import thbFlagImage from '../flags/Thailand.svg';
+import zarFlagImage from '../flags/SouthAfrica.svg';
 
 export const countries = [
   {
@@ -151,5 +165,103 @@ export const countries = [
     flagImage: audFlagImage,
     country: "Australia",
     LCID: "en-AU",
+  },
+  {
+    name: "Brazilian real",
+    shortname: "BRL",
+    flagImage: brlFlagImage,
+    country: "Brazil",
+    LCID: "pt-BR",
+  },
+  {
+    name: "Chinese yuan renminbi",
+    shortname: "CNY",
+    flagImage: cnyFlagImage,
+    country: "China",
+    LCID: "zh-CN",
+  },
+  {
+    name: "Hong Kong dollar",
+    shortname: "HKD",
+    flagImage: hkdFlagImage,
+    country: "Honk Kong",
+    LCID: "zh-HK",
+  },
+  {
+    name: "Indonesian rupiah",
+    shortname: "IDR",
+    flagImage: idrFlagImage,
+    country: "Indonesia",
+    LCID: "id-ID",
+  },
+  {
+    name: "Israeli shekel",
+    shortname: "ILS",
+    flagImage: ilsFlagImage,
+    country: "Israel",
+    LCID: "iw-IL",
+  },
+  {
+    name: "Indian rupee",
+    shortname: "INR",
+    flagImage: inrFlagImage,
+    country: "India",
+    LCID: "en-IN",
+  },
+  {
+    name: "South Korean won",
+    shortname: "KRW",
+    flagImage: krwFlagImage,
+    country: "Republic of South Korea",
+    LCID: "ko-KO",
+  },
+  {
+    name: "Mexican peso",
+    shortname: "MXN",
+    flagImage: mxnFlagImage,
+    country: "Mexico",
+    LCID: "es-MX",
+  },
+  {
+    name: "Malaysian ringgit",
+    shortname: "MYR",
+    flagImage: myrFlagImage,
+    country: "Malaysia",
+    LCID: "ms-MY",
+  },
+  {
+    name: "New Zealand Dollar",
+    shortname: "NZD",
+    flagImage: nzdFlagImage,
+    country: "NewZealand",
+    LCID: "en-NZ",
+  },
+  {
+    name: "Phlilippine peso",
+    shortname: "PHP",
+    flagImage: phpFlagImage,
+    country: "Philippines",
+    LCID: "en-PH",
+  },
+  {
+    name: "Singapore dollar",
+    shortname: "SGD",
+    flagImage: sgdFlagImage,
+    country: "Singapore",
+    LCID: "zh-SG",
+  },
+  {
+    name: "Thai baht",
+    shortname: "THB",
+    flagImage: thbFlagImage,
+    country: "Thailand",
+    LCID: "th-TH",
+  },
+  {
+    name: "South Afrifcan rand",
+    shortname: "ZAR",
+    flagImage: zarFlagImage,
+    country: "Republic of South Africa",
+    LCID: "en-ZA",
   },
 ];
