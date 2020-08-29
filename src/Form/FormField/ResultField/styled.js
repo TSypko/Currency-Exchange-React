8fc2d879945e84658cbd0bfb79d8155bc3c76b84
Tomasz Sypko <tsypko@gmail.com>
2020-08-29 11:05:05 +0200

    changed result display format

--- src/Form/FormField/ResultField/styled.js
+++ src/Form/FormField/ResultField/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Result = styled.p`
     padding: 10px 20px;
@@ -9,6 +9,11 @@ export const Result = styled.p`
     border-radius: 5px;
     font-weight: 400;
     text-transform: uppercase;
+    text-align: center;
+    ${({ center }) => center && css`
+
+    `}
+
 `;
 
 export const Value = styled.span`
